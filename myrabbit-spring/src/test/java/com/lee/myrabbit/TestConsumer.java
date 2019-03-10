8521ac0c1778e969a79096a915e74f4a0e9d3dfb
Phideon <a2z0to9@outlook.com>
2019-03-10 14:01:01 +0800

    user register, save db, send mail asynchronous.

--- myrabbit-spring/src/test/java/com/lee/myrabbit/TestConsumer.java
+++ myrabbit-spring/src/test/java/com/lee/myrabbit/TestConsumer.java
@@ -8,11 +8,12 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 @ContextConfiguration(locations = {"classpath:spring/spring-consumer.xml"})
 @RunWith(SpringJUnit4ClassRunner.class)
 public class TestConsumer {
-	@Test
-	public void testConsumer() {
-		try {
-			Thread.sleep(Long.MAX_VALUE); // 启动消费容器
-		} catch (InterruptedException e) {
-		}
-	}
+    @Test
+    public void testConsumer() {
+        try {
+            Thread.sleep(Long.MAX_VALUE); // 启动消费容器
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
 }
